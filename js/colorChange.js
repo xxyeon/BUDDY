b9257ddfb993e30a983bde9ec11ceca6f1a795de
xxyeon <jykim9335@gmail.com>
2023-03-05 16:36:58 +0900

    colorChange.js 페이지에 버튼 유뮤 확인 하는 로직 추가

--- js/colorChange.js
+++ js/colorChange.js
@@ -1,8 +1,6 @@
 const gameContents = document.querySelector("#game");
 const icon = document.querySelector("#icon");
 const themaInContents = document.querySelector("dd");
-const resultButton = document.querySelector("button")
-
 const line = document.querySelector("#line");
 const buddy = document.querySelectorAll("h1");
 const themaInLogo = document.querySelector("h5");
@@ -14,22 +12,47 @@ const YELLO = "혼자 놀기";
 const CLASSOFGREEN = "iceBreaking-logo";
 const CLASSOFYELLO = "soloing-logo";
 
-if(thema === GREEN) {
-    gameContents.className = "icBreaking-contents";
-    icon.className = CLASSOFGREEN;
-    themaInContents.className = CLASSOFGREEN;
-    buddy[0].className = CLASSOFGREEN;
-    buddy[2].className = CLASSOFGREEN;
-    themaInLogo.className = CLASSOFGREEN;
-    line.className = CLASSOFGREEN;
-    resultButton.classList.toggle("icBreaking-contents");
-} else if(thema === YELLO) {
-    gameContents.className = "soloing-contents";
-    icon.className = CLASSOFYELLO;
-    themaInContents.className = CLASSOFYELLO;
-    buddy[0].className = CLASSOFYELLO;
-    buddy[2].className = CLASSOFYELLO;
-    themaInLogo.className = CLASSOFYELLO;
-    line.className = CLASSOFYELLO;
-    resultButton.classList.toggle("soloing-contents");
+if(document.querySelector("button")){
+    const resultButton = document.querySelector("button");
+    const p = document.querySelector("#game p:first-of-type");
+
+    if(thema === GREEN) {
+        gameContents.className = "icBreaking-contents";
+        icon.className = CLASSOFGREEN;
+        themaInContents.className = CLASSOFGREEN;
+        buddy[0].className = CLASSOFGREEN;
+        buddy[2].className = CLASSOFGREEN;
+        themaInLogo.className = CLASSOFGREEN;
+        line.className = CLASSOFGREEN;
+        p.classList.add(CLASSOFGREEN);
+        resultButton.classList.toggle("icBreaking-contents");
+    } else if(thema === YELLO) {
+        gameContents.className = "soloing-contents";
+        icon.className = CLASSOFYELLO;
+        themaInContents.className = CLASSOFYELLO;
+        buddy[0].className = CLASSOFYELLO;
+        buddy[2].className = CLASSOFYELLO;
+        themaInLogo.className = CLASSOFYELLO;
+        line.className = CLASSOFYELLO;
+        p.classList.add(CLASSOFYELLO);
+        resultButton.classList.toggle("soloing-contents");
+    }
+}else {
+    if(thema === GREEN) {
+        gameContents.className = "icBreaking-contents";
+        icon.className = CLASSOFGREEN;
+        themaInContents.className = CLASSOFGREEN;
+        buddy[0].className = CLASSOFGREEN;
+        buddy[2].className = CLASSOFGREEN;
+        themaInLogo.className = CLASSOFGREEN;
+        line.className = CLASSOFGREEN;
+    } else if(thema === YELLO) {
+        gameContents.className = "soloing-contents";
+        icon.className = CLASSOFYELLO;
+        themaInContents.className = CLASSOFYELLO;
+        buddy[0].className = CLASSOFYELLO;
+        buddy[2].className = CLASSOFYELLO;
+        themaInLogo.className = CLASSOFYELLO;
+        line.className = CLASSOFYELLO;
+    }
 }
