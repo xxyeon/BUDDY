11d02047806c18625c49a1645a4bfa29b328d3b0
xxyeon <jykim9335@gmail.com>
2023-03-11 18:56:41 +0900

    링크 공유 추가 구현
    이미지 클릭시 '복사되었습니다' 메시지 알림

--- js/sharedLink.js
+++ js/sharedLink.js
@@ -1,8 +1,19 @@
 const url = window.location.href; // 현재 링크를 가져오는 코드
-console.log(url);
+
+function showText() {
+  const span = document.createElement("span");
+  span.innerText = "복사되었습니다";
+  span.classList.add("copy");
+  gameContents.appendChild(span);
+  setTimeout(() => {
+    gameContents.removeChild(span);
+  }, 3000);
+}
+
 const copyUrl = async () => {
     await navigator.clipboard.writeText(url);
-    alert("URL이 복사되었습니다.");
+    //alert("URL이 복사되었습니다.");
+    showText();
   };
 
 document.querySelector("#game img:first-of-type").addEventListener("click", copyUrl);
